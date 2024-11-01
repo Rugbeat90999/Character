7ddeea512eeee76c183653ad3a3a242393b51938
Rugbeat90999 <rugbeat909@gmail.com>
2024-11-01 16:52:36 -0500

    start on armor in items

--- registers.py
+++ registers.py
@@ -7,34 +7,53 @@ from CommonLib.functional_classes import staticproperty, staticstr
 
 
 
+class Items(metaclass=staticstr):
+    '''
+    All items that can be used or held by players
+    '''
+    def __str__():
+        pass
+    
+    class Wearable(metaclass=staticstr):
+        pass
+    
+
+    class Useable(metaclass=staticstr):
+        pass
+
+
+    class Other(metaclass=staticstr):
+        pass
+    
+
+
+
 class Item:
-    def __init__(self, name:str, id:int, description:str):
-        self.name = name
-        self.id = id
-        self.description = description
-    
-    def register(self, name:str, id:int, description:str):
-        self.name = name
-        self.id = id
-        self.description = description
+    def __init__(self, uuid:UUID):
+        self.__uuid = uuid
+        self.name = "N/A"
+        self.description = "N/A"
+        self.__regitry_name = uuid.alphabetic_version
+        self.value = 0
+        self.weight = 0
 
-    
-    class Armor():
-        '''
-        All clothing items that provide some protect or special effect
-        '''
-        # def __init_subclass__(cls, slash_rating:int, peirce_rating:int, weight:int, body_location:str):
-        #     super.__init__()
-        #     body_locations = ["feet", "legs", "body", "head", "hands"]
-        #     if body_location not in body_locations:
-        #         raise ValueError(f"Invalid body location: \"{body_location}\". Must be one of \"{body_locations}\".")
-        #     cls.slash_rating = slash_rating
-        #     cls.peirce_rating = peirce_rating
-        #     cls.weight = weight
-        #     cls.body_location = body_location
+    def __str__(self):
+        # TODO: add this
+        return "add this str"
 
-        def __init_subclass__(cls):
-            cls.items = []
+
+
+class Armor(Item):
+        '''
+        Wearable items that provide protect
+        '''
+
+        def __init__(self, uuid:UUID):
+            super().__init__(uuid)
+            self.slash_rating = 0
+            self.peirce_rating = 0
+            self.weight = 0
+            self.body_location = ""
 
 
         def register(cls, slash_rating:int, peirce_rating:int, weight:int, body_location:str):
@@ -48,68 +67,64 @@ class Item:
             cls.body_location = body_location
 
 
-    class Clothes:
-        '''
-        All clothing wore entirely for athstetic
-        '''
-        def __init__(self):
-            pass
-          
-          
-    class Trinkets:
-        '''
-        Small wearable items such as necklaces, or ring
-        '''
-        def __init__(self):
-            self.right_glove = None
-            self.left_glove = None
-            self.right_wrist = None
-            self.left_wrist = None
-            self.right_ankle = None
-            self.left_ankle = None
-            self.right_biscep = None
-            self.left_biscep = None
-            self.right_thigh = None
-            self.left_thigh = None
-            self.right_ear = None
-            self.left_ear = None
-            self.eyes = None
-            self.neck = None
-
-          
-    class Weapons:
-        '''
-        Items that have offensive value 
-        '''
-        def __init__(self):
-            pass
-
-
-    class Tools:
-        '''
-        Items that have a special use, such a picks, and rope.
-        '''
-        def __init__(self):
-            pass
-        
-        
+class Clothes:
+    '''
+    All clothing wore entirely for athstetic
+    '''
     def __init__(self):
-        self.general = list[Item]()
-    
-   
-    class Money:
-        def __init__(self):
-            pass
+        pass
           
-   
-    class QuestItems:
-        def __init__(self):
-            pass
           
+class Trinkets:
+    '''
+    Small wearable items such as necklaces, or ring
+    '''
+    def __init__(self):
+        self.right_glove = None
+        self.left_glove = None
+        self.right_wrist = None
+        self.left_wrist = None
+        self.right_ankle = None
+        self.left_ankle = None
+        self.right_biscep = None
+        self.left_biscep = None
+        self.right_thigh = None
+        self.left_thigh = None
+        self.right_ear = None
+        self.left_ear = None
+        self.eyes = None
+        self.neck = None
 
-    class General:
-                def __init__(self):
-                    pass
+          
+class Weapons:
+    '''
+    Items that have offensive value 
+    '''
+    def __init__(self):
+        pass
+
+
+class Tools:
+    '''
+    Items that have a special use, such a picks, and rope.
+    '''
+    def __init__(self):
+        pass
+
+
+class Money:
+    def __init__(self):
+        pass
+
+
+class QuestItems:
+    def __init__(self):
+        pass
+
+
+class General:
+            def __init__(self):
+                pass
 
 
 
@@ -876,7 +891,3 @@ class PermanentEffect(Effect):
         self.intelligence = 0
         self.charisma = 0
         self.wereworlf = False
-
-
-
-
